2093725db29aaa54c853ebe8daa05ac39c63ffd4
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-03-28 04:18:18 -0700

    [Fix]:Fixing bugs

--- print_string.c
+++ print_string.c
@@ -3,22 +3,28 @@
 
 /**
  * print_string - print string.
- *
- * @s: argument.
+ * @arg_list: list of argument
+ * @buf: pointer to buffer
+ * @ibuf: current index of buffer
  *
  * Return: character count.
  */
 
-int print_string(char *s)
+int print_string(va_list arg_list, char *buf, unsigned int ibuf)
 {
-	int i;
-	int count_fun = 0;
+	char *s, nill[] = "(null)";
+	unsigned int i;
 
-	if (s[0] == '\0')
-		return (-1);
+	s = va_arg(arg_list, char *);
+	if (s == NULL)
+	{
+		for (i = 0; nill[i]; i++)
+			ibuf = handle_buf(buf, nill[i], ibuf);
+		return (6);
+	}
 
 	for (i = 0; s[i] != '\0'; i++)
-		count_fun += _putchar(s[i]);
+		ibuf = handle_buf(buf, s[i], ibuf);
 
-	return (count_fun);
+	return (i);
 }
