2d6675547f414123c188c05f5f04cd0ffbb3a62f
batoolharah <batooladnanharah@gmail.com>
2023-03-27 18:09:02 -0700

    header file

--- main.h
+++ main.h
@@ -5,10 +5,15 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+
+/*prototypes*/
 int _printf(const char *format, ...);
 int _putchar(char c);
 
+/*Conversion specifiers*/
 int print_char(va_list args);
 int print_string(va_list args);
+int print_int(int i);
+
 
 #endif
