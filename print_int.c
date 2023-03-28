32d2f0a6d9f8603e572901387087afc516849a75
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-03-28 04:19:30 -0700

    [Fix]: Fixing bugs

--- print_int.c
+++ print_int.c
@@ -2,18 +2,22 @@
 
 /**
 * print_int - prints integer
-* @i: number to be printed
+* @arg_list: argument list
+* @buf: pointer to buffer
+* @ibuf: current index of pointer
 *
 * Return: number of characters printed.
 */
-int print_int(int i)
+int print_int(va_list arg_list, char *buf, unsigned int ibuf)
 {
-	int int_in, count = 0, isneg = 0, j;
+	int int_in, isneg = 0, j, i;
 	unsigned int temp, div;
 
+	i = va_arg(arg_list, int);
 	if (i < 0)
 	{
 		int_in = i * -1;
+		ibuf = handle_buf(buf, '-', ibuf);
 		isneg = 1;
 	}
 	else
@@ -25,9 +29,8 @@ int print_int(int i)
 		div *= 10;
 		temp /= 10;
 	}
-	if (isneg)
-		count += _putchar('-');
+
 	for (j = 0; div > 0; div /= 10, j++)
-		count += _putchar((int_in / div) % 10 + '0');
-	return (count);
+		ibuf = handle_buf(buf, (int_in / div) % 10 + '0', ibuf);
+	return (j + isneg);
 }
