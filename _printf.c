b1bd0797eadf6aae614d9d44dd1ebbc4c392bfa6
batoolharah <batooladnanharah@gmail.com>
2023-03-28 17:31:21 -0700

    [Fix]: Fix bugs and Update file

--- _printf.c
+++ _printf.c
@@ -3,22 +3,21 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - prints a string in the stdout following a format
- * @format: string that specifies the format
- *
- * Return: the number of character printed in the stdout
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list arg_list;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(arg_list, format);
+	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -34,11 +33,11 @@ int _printf(const char *format, ...)
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, arg_list);
-			precision = get_precision(format, &i, arg_list);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, arg_list, buffer,
+			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
@@ -48,7 +47,7 @@ int _printf(const char *format, ...)
 
 	print_buffer(buffer, &buff_ind);
 
-	va_end(arg_list);
+	va_end(list);
 
 	return (printed_chars);
 }
